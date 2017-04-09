1deeb566326058f9e3cbae09775c48e90ccc908b
linuxdaemon <walter@walterbarnes.net>
2017-04-09 08:28:52 -0500

    Clean up netstats.py

--- netstats.py
+++ netstats.py
@@ -9,49 +9,52 @@ from libhex import hook
 
 __module_name__ = "NetStats"
 __module_description__ = "Network Statistics"
-__module_version__ = "0.1"
+__module_version__ = "0.0.2"
 
 data = {}
 
 
 @hook.server("254")
-def on254(word, word_eol, userdata):
+def on_global_chans(word, word_eol, userdata):
     net = hexchat.get_info("network").lower()
     if data.get(net, {}).get("enabled"):
-        data.setdefault(net, {})["numchannels"] = int(word[3])
-        if "state" not in data[net]:
-            data[net]["state"] = 0
-
-        data[net]["state"] += 1
-        if data[net]["state"] > 1:
-            data[net]["state"] = 0
-            print_stats(data[net])
+        data[net]["numchannels"] = int(word[3])
+        check_state(net)
 
 
 @hook.server("266")
-def on266(word, word_eol, userdata):
+def on_user_count(word, word_eol, userdata):
     net = hexchat.get_info("network").lower()
     if data.get(net, {}).get("enabled"):
-        data.setdefault(net, {})["numusers"] = int(word[6])
-        if "state" not in data[net]:
-            data[net]["state"] = 0
+        data[net]["numusers"] = int(word[6])
+        check_state(net)
 
-        data[net]["state"] += 1
-        if data[net]["state"] > 1:
-            data[net]["state"] = 0
-            print_stats(data[net])
+
+def check_state(net):
+    if "state" not in data[net]:
+        data[net]["state"] = 0
+
+    data[net]["state"] += 1
+    if data[net]["state"] > 1:
+        data[net]["state"] = 0
+        print_stats(data[net])
+        del data[net]
 
 
 def print_stats(netdata):
     netdata["enabled"] = False
     ctx = netdata["ctx"]
     network = ctx.get_info("network")
+
     visiblechans = [chan for chan in ctx.get_list("channels") if chan.network == network and chan.type == 2]
     visibleusers = set(user.nick for chan in visiblechans for user in chan.context.get_list("users"))
+
     numchans = len(visiblechans)
     numusers = len(visibleusers)
+
     maxchans = netdata["numchannels"]
     maxusers = netdata["numusers"]
+
     fmt_data = {
         "numchans": numchans,
         "maxchans": maxchans,
@@ -65,7 +68,7 @@ def print_stats(netdata):
           "Users: {maxusers} (share a channel with {numusers} [{pctusers:.2g}%])"
 
     if netdata.get("say"):
-        ctx.command("say " + fmt.format(**fmt_data))
+        ctx.command("say {}".format(fmt.format(**fmt_data)))
     else:
         print(fmt.format(**fmt_data))
 
@@ -75,7 +78,7 @@ def stats_cmd(word, word_eol, userdata):
     net = hexchat.get_info("network")
     data[net.lower()] = {
         "ctx": hexchat.find_context(net, hexchat.get_info("channel")),
-        "say": len(word) > 1 and "-o" in word[1:],
+        "say": "-o" in word[1:],
         "enabled": True
     }
 
