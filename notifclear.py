c7163b43d9fc0a66489426e19be911fa94ed62a2
linuxdaemon <walter@walterbarnes.net>
2017-05-27 05:33:35 -0500

    Remove custom lib dependency from notifclear.py

--- notifclear.py
+++ notifclear.py
@@ -1,18 +1,11 @@
-import os
-import sys
-
+# coding=utf-8
 import hexchat
-
-sys.path = [os.path.join(hexchat.get_info("configdir"), "addons")] + sys.path
-
-from libhex import hook
 
 __module_name__ = "NotificationClear"
 __module_description__ = "Clears all notifications in all contexts"
-__module_version__ = "0.1"
+__module_version__ = "0.1.0"
 
 
-@hook.command("CLEARNOTIF")
 def clear(word, word_eol, userdata):
     for chan in hexchat.get_list("channels"):
         chan.context.command("GUI COLOR 0")
@@ -20,9 +13,11 @@ def clear(word, word_eol, userdata):
     return hexchat.EAT_ALL
 
 
-@hook.unload
-def unload(userdata):
-    print(__module_name__, "plugin unloaded")
-
+hexchat.hook_unload(lambda userdata: print(__module_name__, "plugin unloaded"))
+hexchat.hook_command(
+    "CLEARNOTIF", clear,
+    help="Clear all context highlights, setting them back to the default tab "
+         "color"
+)
 
 print(__module_name__, "plugin loaded")
